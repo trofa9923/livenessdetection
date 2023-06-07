277fbeb42966777b9acd8759f96cc6b1cd57b5f3
Ian Managuelod <73928922+trofa9923@users.noreply.github.com>
2023-06-07 09:54:58 +0800

    UPDATE

--- script.js
+++ script.js
@@ -55,8 +55,48 @@ function downloadRecording() {
     link.download = 'recorded_video.webm';
     link.click();
     showMessage('Video downloaded');
+
+    blobToBase64(blob).then(
+      (blobtry)=>{		 
+       let xhr = new XMLHttpRequest();
+      //  var baseUrl = (window.location).href; // You can also use document.URL
+      //  var koopId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1, baseUrl.indexOf('&'));
+      //  var kooptbl = baseUrl.substring(baseUrl.lastIndexOf('&') + 1);
+       var baseUrl = '1'
+       var koopId = '2'
+       var kooptbl = '3'
+       xhr.open("POST", "https://prod-60.southeastasia.logic.azure.com:443/workflows/f4698ff8f7584080bc2abe9fb7efbf09/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=smIe_NUVgekY5xEJ-RcNRA-CKf1hnZNBKHEdDvqp6Ac");
+       xhr.setRequestHeader("Accept", "application/json");
+       xhr.setRequestHeader("Content-Type", "application/json");
+       xhr.onreadystatechange = function () {
+         if (xhr.readyState === 4) {
+           console.log(xhr.status);
+           console.log(xhr.responseText);
+         }};
+ 
+       let data = `{
+         "imageUrl": "${blobtry}",
+         "mobile": "${koopId}",
+         "table" : "${kooptbl}",
+         "status": "DONE",
+         "ext": "mp4"
+       }`;
+ 
+       xhr.send(data);
+     }
+   )    
+
   };
   reader.readAsDataURL(blob);
+}
+
+function blobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = () => reject(reader.error);
+    reader.readAsDataURL(blob);
+  });
 }
 
 
